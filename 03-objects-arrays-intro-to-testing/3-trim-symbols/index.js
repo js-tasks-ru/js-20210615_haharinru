ed1ead223745b96d4cf8e7efc01891307ad5d7ce
Ivan Turchinov <Haharinru@yandex.ru>
2021-06-25 16:58:30 +0300

    task 3.3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,31 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (size === undefined) {
+    return string;
+  }
+  let arrOfSymbols = Array.from(string);
+  let initCounter = {prevSymbol: ``, count: 0, result: ``, prevAdded: false};
+  return arrOfSymbols.reduce((counter, curSymbol) => {
+    let sameSymbol = counter.prevSymbol === curSymbol;
+    if (sameSymbol) {
+      if (!counter.prevAdded) {
+        return {...counter};
+      }
+      let shouldAdd = counter.count + 1 <= size;
+      return {...counter,
+        count: counter.count + 1,
+        result: shouldAdd ? counter.result + curSymbol : counter.result,
+        prevAdded: shouldAdd,
+      };
+    } else {
+      let shouldAdd = 1 <= size;
+      return {...counter,
+        prevSymbol: curSymbol,
+        count: 1,
+        result: shouldAdd ? counter.result + curSymbol : counter.result,
+        prevAdded: shouldAdd
+      };
+    }
+  }, initCounter).result;
 }
